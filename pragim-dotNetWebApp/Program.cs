c413f01920a433fd492697184dcbdf35ea9b868d
VisualStudio2022 <vs2022@mail.it>
2025-03-21 08:55:32 +0700

    Add project files.

---
+++ pragim-dotNetWebApp/Program.cs
@@ -0,0 +1,15 @@
+namespace pragim_dotNetWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
