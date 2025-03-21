3152894b050636d34aa4358b263cdca786e3ea98
VisualStudio2022 <vs2022@mail.it>
2025-03-21 11:10:37 +0700

    9 ASP NET Core appsettings json file

--- pragim-dotNetWebApp/Program.cs
+++ pragim-dotNetWebApp/Program.cs
@@ -1,15 +1,16 @@
-namespace pragim_dotNetWebApp
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+using Microsoft.AspNetCore.Builder;
 
-            app.MapGet("/", () => "Hello World!");
-
-            app.Run();
-        }
+namespace pragim_dotNetWebApp {
+  public class Program {
+    public static void Main(string[] args) {
+      /* =================================== services =================================== */
+      var builder = WebApplication.CreateBuilder(args);
+      string? myKey = builder.Configuration["MyKey"];
+      /* =================================== pipeline =================================== */
+      var app = builder.Build();
+      app.UseDeveloperExceptionPage();
+      app.MapGet("/", () => myKey);
+      app.Run();
     }
+  }
 }
