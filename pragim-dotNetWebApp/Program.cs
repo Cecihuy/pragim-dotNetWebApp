306a4f0bd165d55e210b2e9f6dae58bd4d3018ce
VisualStudio2022 <vs2022@mail.it>
2025-03-21 18:09:57 +0700

    11 Configure ASP NET Core request processing pipeline

--- pragim-dotNetWebApp/Program.cs
+++ pragim-dotNetWebApp/Program.cs
@@ -1,15 +1,29 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
 
 namespace pragim_dotNetWebApp {
   public class Program {
     public static void Main(string[] args) {
       /* =================================== services =================================== */
       var builder = WebApplication.CreateBuilder(args);
-      string? myKey = builder.Configuration["MyKey"];
       /* =================================== pipeline =================================== */
       var app = builder.Build();
-      app.UseDeveloperExceptionPage();
-      app.MapGet("/", () => myKey);
+      app.Use(async (context, next) => {
+        app.Logger.LogInformation("MW1: Incoming Request");
+        await next();
+        app.Logger.LogInformation("MW1: Outgoing Response");
+      });
+      app.Use(async (context, next) => {
+        app.Logger.LogInformation("MW2: Incoming Request");
+        await next();
+        app.Logger.LogInformation("MW2: Outgoing Response");
+      });
+      app.MapGet("/", async (context) => {
+        await context.Response.WriteAsync("MW3: Request handled and response produced");
+        app.Logger.LogInformation("MW3: Request handled and response produced");
+      });
       app.Run();
     }
   }
