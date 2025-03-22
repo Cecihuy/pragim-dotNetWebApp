b43738f089978b4b6b8e7d82d36f49429e7dd54f
VisualStudio2022 <vs2022@mail.it>
2025-03-22 14:15:14 +0700

    16 Setup mvc in asp net core

---
+++ pragim-dotNetWebApp/Controllers/HomeController.cs
@@ -0,0 +1,7 @@
+﻿namespace pragim_dotNetWebApp.Controllers {
+  public class HomeController {
+    public string Index() {
+      return "Hello from MVC";
+    }
+  }
+}
--- pragim-dotNetWebApp/Program.cs
+++ pragim-dotNetWebApp/Program.cs
@@ -1,21 +1,29 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
 
 namespace pragim_dotNetWebApp {
   public class Program {
     public static void Main(string[] args) {
       /* =================================== variable =================================== */
+      
       /* =================================== services =================================== */
       var builder = WebApplication.CreateBuilder(args);
+      builder.Services.Configure<MvcOptions>(options => {
+        options.EnableEndpointRouting = false;
+      });
+      builder.Services.AddMvc();
       /* =================================== pipeline =================================== */
       var app = builder.Build();
       if(app.Environment.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
       app.UseStaticFiles();
+      app.UseMvcWithDefaultRoute();
       app.Use(async (context, next) => {
-        await context.Response.WriteAsync("Hosting Environtment: " + app.Environment.EnvironmentName);
+        await context.Response.WriteAsync("Hello World");
         await next();
-      });      
+      });
       app.Run();
     }
   }
