62fd4b3cc84802e88f414afc89b234029bd848ca
VisualStudio2022 <vs2022@mail.it>
2025-03-22 12:52:41 +0700

    13 ASP NET Core developer exception page

--- pragim-dotNetWebApp/Program.cs
+++ pragim-dotNetWebApp/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 
@@ -5,18 +6,24 @@ namespace pragim_dotNetWebApp {
   public class Program {
     public static void Main(string[] args) {
       /* =================================== variable =================================== */
-      FileServerOptions fileServerOptions = new FileServerOptions();
-      fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
-      fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("foo.html");
+      ExceptionHandlerOptions exceptionHandlerOptions = new ExceptionHandlerOptions { 
+        ExceptionHandlingPath = "/foo.html" // if an error occured, go to this path
+      };
+      DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions {
+        SourceCodeLineCount = 10
+      };
       /* =================================== services =================================== */
-      var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
       /* =================================== pipeline =================================== */
       var app = builder.Build();
-      app.UseFileServer(fileServerOptions);
+      app.UseExceptionHandler(exceptionHandlerOptions);
+      app.UseDeveloperExceptionPage(developerExceptionPageOptions);
+      app.UseFileServer();
       app.Use(async (context, next) => {
+        throw new Exception("Some error processing the request");
         await context.Response.WriteAsync("Hello World");
         await next();
-      });
+      });      
       app.Run();
     }
   }
