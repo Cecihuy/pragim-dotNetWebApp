c028a1cd70656f8932c4d29dc6ce9d410bafdea6
VisualStudio2022 <vs2022@mail.it>
2025-03-26 11:40:53 +0700

    62 Logging exceptions in ASP NET Core

--- pragim-dotNetWebApp/Controllers/ErrorController.cs
+++ pragim-dotNetWebApp/Controllers/ErrorController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace pragim_dotNetWebApp.Controllers {
   public class ErrorController : Controller {
+    private readonly ILogger<ErrorController> logger;
+
+    public ErrorController(ILogger<ErrorController> logger) {
+      this.logger=logger;
+    }
     [Route("error/{statusCode}")]
     public IActionResult HttpStatusCodeHandler(int statusCode) {
       IStatusCodeReExecuteFeature? statusCodeReExecuteFeature = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
@@ -11,7 +17,10 @@ namespace pragim_dotNetWebApp.Controllers {
         case 404:
           ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found";
           ViewBag.Path = statusCodeReExecuteFeature.OriginalPath;
-          ViewBag.QueryString = statusCodeReExecuteFeature.OriginalQueryString;
+          logger.LogWarning(
+            $"404 error occured. Path = {statusCodeReExecuteFeature.OriginalPath}" +
+            $"and Query String = {statusCodeReExecuteFeature.OriginalQueryString}"
+          );
           break;
       }
       return View("notFound");
@@ -20,9 +29,7 @@ namespace pragim_dotNetWebApp.Controllers {
     [AllowAnonymous]
     public IActionResult Error() {
       IExceptionHandlerFeature? exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
-      ViewBag.ExceptionPath = exceptionHandlerFeature.Path;
-      ViewBag.ExceptionMessage = exceptionHandlerFeature.Error.Message;
-      ViewBag.StackTrace = exceptionHandlerFeature.Error.StackTrace;
+      logger.LogError($"The path {exceptionHandlerFeature.Path} threw an exception {exceptionHandlerFeature.Error}");
       return View("error");
     }
   }
