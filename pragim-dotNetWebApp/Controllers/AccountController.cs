5eb333a69eb1a4f4588b84a802ba6de2a26be8be
VisualStudio2022 <vs2022@mail.it>
2025-03-27 10:36:35 +0700

    66 Register new user using asp net core identity

---
+++ pragim-dotNetWebApp/Controllers/AccountController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pragim_dotNetWebApp.Controllers {
+  public class AccountController : Controller {
+    [HttpGet]
+    public IActionResult Register() {
+      return View();
+    }
+  }
+}
---
+++ pragim-dotNetWebApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pragim_dotNetWebApp.ViewModels {
+  public class RegisterViewModel {
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm Password")]
+    [Compare("Password", ErrorMessage = "Password does not match")]
+    public string ConfirmPassword { get; set; }
+  }
+}
