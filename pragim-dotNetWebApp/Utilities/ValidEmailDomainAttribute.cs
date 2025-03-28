1b0a44054dd969c15d4d827da341c8134fe0a54f
VisualStudio2022 <vs2022@mail.it>
2025-03-28 08:35:19 +0700

    76 Custom validation attribute in asp net core

---
+++ pragim-dotNetWebApp/Utilities/ValidEmailDomainAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pragim_dotNetWebApp.Utilities {
+  public class ValidEmailDomainAttribute : ValidationAttribute {
+    private readonly string allowedDomain;
+
+    public ValidEmailDomainAttribute(string allowedDomain) {
+      this.allowedDomain=allowedDomain;
+    }
+    // object? value parameter automatically added from Email input
+    public override bool IsValid(object? value) {
+      string[] strings = value.ToString().Split('@');
+      return strings[1].ToUpper() == allowedDomain.ToUpper();
+    }
+  }
+}
--- pragim-dotNetWebApp/ViewModels/RegisterViewModel.cs
+++ pragim-dotNetWebApp/ViewModels/RegisterViewModel.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using pragim_dotNetWebApp.Utilities;
 using System.ComponentModel.DataAnnotations;
 
 namespace pragim_dotNetWebApp.ViewModels {
   public class RegisterViewModel {
     [Required]
     [EmailAddress]
+    [ValidEmailDomain(allowedDomain: "pragimtech.com", 
+      ErrorMessage = "Email domain must be pragimtech.com")]
     [Remote("isemailinuse","account")]
     public string Email { get; set; }
-    [Required]
-    [DataType(DataType.Password)]
+    [Required][DataType(DataType.Password)]
     public string Password { get; set; }
     [DataType(DataType.Password)]
     [Display(Name = "Confirm Password")]
